7142dfbc80827e0440e1d457c5b45bb722e3c23d
Athul-krish <athulkrishnapanamakkal@gmail.com>
2024-05-25 13:22:49 +0530

    first commit

---
+++ src/Components/AddBlog.jsx
@@ -0,0 +1,20 @@
+import { Button, TextField, Typography } from '@mui/material'
+import React from 'react'
+
+
+const AddBlog = () => {
+  return (
+    <div>
+        
+        <Typography variant='h3'style={{fontFamily:"cursive"}}></Typography>
+        <Typography variant='h4' style={{color:'darkslateblue', fontFamily:'fantasy'}}>Blog Form</Typography><br/><br/>
+        <TextField variant='outlined' label='Blog Name' style={{color:'red', fontFamily:'fantasy'}}></TextField><br/><br/>
+        <TextField variant='outlined' label='Description' style={{color:'red', fontFamily:'fantasy'}}></TextField><br/><br/>
+        <TextField variant='outlined' label='Author Name' style={{color:'red', fontFamily:'fantasy'}}></TextField><br/><br/>
+        <Button variant='contained' style={{fontFamily:'revert'}}>ENTER</Button>
+      
+    </div>
+  )
+}
+
+export default AddBlog
